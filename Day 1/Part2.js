2257f615e44b88815615b24733aac7469a82a62b
Jaycee Barrameda <jmbarrameda@up.edu.ph>
2020-12-02 12:46:23 +0800

    added Day1

--- Day 1/Part2.js
+++ Day 1/Part2.js
@@ -4,9 +4,13 @@ fs.readFile("./input.txt", (err,data) => {
     console.log(err)    
   } else { 
     const input = data.toString().trim().split("\n").map(x=>+x);
-    arr = input.flatMap( (e) => {
-        return input.includes(2020-e) ? (2020-e)*e : [];
-      })
+    arr = input.flatMap( (x) => {
+        const z = 2020-x;
+        const arr2 = input.flatMap( (y) => {
+          return input.includes(z-y) ? y*x*(z-y) : [];
+        })
+        return arr2
+      })     
       var ans = Math.max(...arr)
       console.log(ans)
   } 
